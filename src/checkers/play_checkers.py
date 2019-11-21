942a0f286317eab8929f1d3727fd8f56a04e3447
Robert Schloen <rschloen@gmail.com>
2019-11-21 09:49:47 -0600

    Added new functions to AI class
    Functions: check win condition, switch turn

--- src/checkers/play_checkers.py
+++ src/checkers/play_checkers.py
@@ -1,8 +1,28 @@
-import math
 
 
 class CheckersAI():
+    '''Keeps track of game progress and uses Minimax with alpha beta pruning to find best move for the Baxter robot'''
     def __init__(self):
         self.game_over = False
         self.whos_turn = 'Red'
         self.noCaptureCounter = 0
+        self.red_piece_count = 12
+        self.black_piece_count = 12
+        self.winner = None
+
+
+    def switch_turn(self):
+
+        if self.whos_turn == 'Red':
+            self.whos_turn = 'Black'
+        else:
+            self.whos_turn = 'Red'
+
+    def is_game_over(self):
+        if self.red_piece_count == 0:
+            self.winner = 'Black'
+            self.game_over = True
+
+        if self.black_piece_count == 0:
+            self.winner = 'Red'
+            self.game_over = True
